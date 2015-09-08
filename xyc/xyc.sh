7083a8ba729aed24cd514791f26563f2cb5e7df5
GiovanniGiorgi <giova96g16@gmail.com>
2015-09-08 03:30:17 +0200

    Update xyc.sh
    add a description on top of the script

--- xyc/xyc.sh
+++ xyc/xyc.sh
@@ -1,3 +1,14 @@
+# Description:
+# This script runs inside the Xiaomi Yi camera and allows to
+# enable RAW file creation and change photographic options such as
+# Exposure, ISO, White Balance, Noise Reduction and Scene
+#
+# Usage:
+# 1) copy the script on the root of SD Card
+# 2) enable wifi and connect to the camera using telnet
+#    (IP:192.168.42.1; port:23; User:root)
+# 3) Run /tmp/fuse_d/xyc.sh
+
 FUSED=/tmp/fuse_d
 
 welcome ()
